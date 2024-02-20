a2bd98dc924e8759a358e6aeee16fc633ee3fb7e
Vishrut2302 <vishrut.raut@saama.com>
2024-02-20 07:51:59 +0530

    firstCommit

---
+++ src/main/java/com/demo/second/Demo1Application.java
@@ -0,0 +1,13 @@
+package com.demo.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/second/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
